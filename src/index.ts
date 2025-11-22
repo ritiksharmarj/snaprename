cf4ad5ee1124df5d792d8224ba3e61168d231638
ritiksharmarj <theonlyritik@gmail.com>
2025-11-23 01:16:45 +0530

    feat: implement basic CLI structure with commander

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("WOW!!!!!!!");
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .name("snaprename")
+  .description("Rename your screenshots with AI")
+  .version("1.0.0");
+
+program
+  .argument("<string>", "string to log")
+  .action((str: string) => console.log(`hello ${str}`));
+
+program.parse();
